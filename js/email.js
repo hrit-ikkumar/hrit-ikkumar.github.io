f99ae48b4a50d237c806f14767e6c0baab61070b
Hritik Kumar Sharma <44857105+hritikkumaar@users.noreply.github.com>
2019-08-02 09:46:23 +0530

    Add files via upload

---
+++ js/email.js
@@ -0,0 +1,10 @@
+function check()
+{
+	var email1 = document.getElementById('email_addr');
+	var email2 = document.getElementById('email_repeat');
+	if(email1.value != email2.value){
+		alert("Emails must match!!");
+		return false;
+	}
+	return true;
+}
---
+++ js/me.js
@@ -0,0 +1,14 @@
+function nicknamefunction()
+{
+	if(document.getElementById('yesNick').checked){
+		document.getElementById('nick').style.display = "inline";
+		document.getElementById('nickname').setAttribute('required', true);
+	}
+
+	else
+	{
+		
+		document.getElementById('nickname').removeAttribute('required');
+		document.getElementById('nick').style.display = "none";
+	}
+}
